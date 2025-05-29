6c8c3b916e74fc4ebb9437c7c8e378a1664f7065
Arthur Leão <arthur.leao@a.ucb.br>
2025-05-29 20:31:43 -0300

    Aula de teste para implementar no projeto de teste

---
+++ sum.test.js
@@ -0,0 +1,17 @@
+const sum = require('./sum');
+
+test('soma 1 + 2 para igualar 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('soma números negativos', () => {
+  expect(sum(-1, -2)).toBe(-3);
+});
+
+test('soma 2 + 2 para igualar 4', () => {
+  expect(sum(2, 2)).toBe(4);
+});
+
+test('soma 2 + 2 para igualar 5', () => {
+  expect(sum(2, 4)).toBe(5);
+});
